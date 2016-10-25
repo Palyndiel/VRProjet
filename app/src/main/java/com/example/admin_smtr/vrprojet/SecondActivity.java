e8bdec4f3e853d072bd5de3aa0b30946baee416f
Palyndiel <Boldwyrien3>
2016-10-25 15:43:04 +0200

    suppression des submodules

---
+++ app/src/main/java/com/example/admin_smtr/vrprojet/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.admin_smtr.vrprojet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by admin-smtr on 25/10/16.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
